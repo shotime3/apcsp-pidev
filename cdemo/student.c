de3c9f251905fdc2ffc861e6965081b4ab64fd50
shotime3 <shohuang85@gmail.com>
2019-05-27 23:16:26 -0700

    students are dumb

--- cdemo/student.c
+++ cdemo/student.c
@@ -8,10 +8,9 @@ struct Student {
   int studentID;
 };
 
-void printStudent(struct Student* student)
-{
+void printStudent(struct Student* student) {
   printf("Student:\n");
-  printf("Name: %s %s\n", student -> firstName, student -> lastName);
+  printf("Name: %s %s\n",student -> firstName, student -> lastName);
   printf("Age: %d\n", student -> age);
   printf("Student ID: %d\n", student -> studentID);
 }
@@ -23,9 +22,8 @@ int main() {
   fgets(strInput, 256, stdin);
   sscanf(strInput, "%d", &numStudent);
   struct Student student[numStudent];
-  for (int i = 0; i<numStudent; i++)
-  {
-    printf("You are entering student number: %d\n", i);
+  for (int i = 0; i < numStudent; i++) {
+    printf("You are entering student number %d\n", i);
     printf("First Name: ");
     fgets(strInput, 256, stdin);
     sscanf(strInput, "%s", student[i].firstName);
@@ -39,10 +37,7 @@ int main() {
     fgets(strInput, 256, stdin);
     sscanf(strInput, "%d", &student[i].studentID);
   }
-
-
-for (int i = 0; i<numStudent; i++)
-{
-  printStudent(&student[i]);
-}
+  for (int i = 0; i < numStudent; i++) {
+    printStudent(&student[i]);
+  }
 }
